d14f5e94e885e33bd95cdd684c58c8f6301c066c
dafuga <danielfugere28@gmail.com>
2025-08-22 18:14:35 -0700

    fix: handling environments with no localization

--- src/callback.ts
+++ src/callback.ts
@@ -7,19 +7,19 @@ export async function waitForCallback(callbackArgs, buoyWs, t): Promise<Callback
 
     if (!callbackResponse) {
         // If the promise was rejected, throw an error
-        throw new Error(callbackResponse.rejected)
+        throw new Error('Empty callback response received')
     }
+
+    // Process the identity request callback payload
+    const payload = JSON.parse(callbackResponse) as CallbackPayload
 
     // If the promise was rejected, throw an error
     if (typeof callbackResponse.rejected === 'string') {
         throw new Error(callbackResponse.rejected)
     }
 
-    // Process the identity request callback payload
-    const payload = JSON.parse(callbackResponse) as CallbackPayload
-
     if (payload.sa === undefined || payload.sp === undefined || payload.cid === undefined) {
-        throw new Error(t('error.cancelled', {default: 'The request was cancelled from Anchor.'}))
+        throw new Error('The request was cancelled.')
     }
 
     return payload
